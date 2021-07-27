a794a460ad493f21c3ae326915f334a6283f1c27
DogpaChen <dogpa0130@gmail.com>
2021-07-27 14:54:13 +0800

    Initial Commit

---
+++ Swift Practice # 46 ViewDidLoad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift Practice # 46 ViewDidLoad
+//
+//  Created by CHEN PEIHAO on 2021/7/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
